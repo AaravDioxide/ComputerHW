12ee939a9472bf1ca391ba8600cb3b725f629f5f
AaravDioxide <166243248+AaravDioxide@users.noreply.github.com>
2024-08-08 20:05:22 +0530

    Computer hhw

---
+++ Computer.py
@@ -0,0 +1,21 @@
+#Homework : Make a program to print cube using while loop
+while True:
+    a = int(input("Please enter the number\n "))
+    i= input("Please enter your option for what you want to find\n For square write 'square'\n For cube write 'cube'\n ")
+    if i.lower == "square":
+        print (a*a)
+    elif i.lower== "cube":
+        print ("a*a*a")
+    else:
+        print ("Please retry with correct syntax")
+
+# Difference between list and tuple
+'''
+
+L=we use []
+T=we use ()
+L=they are mutable
+T= they are inmultable
+'''
+
+#Aarav Dandona VIII-D 
